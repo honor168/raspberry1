9bf58a9c6d15bbf50968da1fbd8d2ebd406efaf1
Bad Boy <bad@example.com>
2020-02-26 10:07:08 +0800

    web: RGB led

---
+++ RGB.py
@@ -0,0 +1,12 @@
+from gpiozero import Button,RGBLED
+
+button = Button(18)
+rgbLed = RGBLED(17,27,22)
+rgbLed.color = (1, 0, 0)
+while True:
+    if button.is_pressed:
+        print("Button is pressed")
+        rgbLed.color = (0, 0, 0)
+    else:
+        print("Button is not pressed")
+        rgbLed.color = (0, 1, 0)

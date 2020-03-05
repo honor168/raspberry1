d3cb79c007ddbb14160e22304444c8773ce0bb81
Bad Boy <bad@example.com>
2020-03-05 11:19:15 +0800

    modify

---
+++ relayControl/androidProject/app/src/main/java/com/honor168/relaycontrol/RGBActivity.java
@@ -0,0 +1,14 @@
+package com.honor168.relaycontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RGBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r_g_b);
+    }
+}

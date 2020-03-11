ce94046f536b196434ac99ae62a099f8ddb5af1a
Bad Boy <bad@example.com>
2020-03-11 10:27:23 +0800

    firebase Login

--- rfid/AndroidProject/app/src/main/java/com/honor168/relaycontrol/MainActivity.java
+++ rfid/AndroidProject/app/src/main/java/com/honor168/relaycontrol/MainActivity.java
@@ -1,6 +1,7 @@
 package com.honor168.relaycontrol;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,7 +11,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;   //
 import com.google.firebase.auth.FirebaseUser;   //
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance(); //
 
+        /*
         firestore.collection("Doors").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() { //2
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -33,6 +38,32 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     Log.d("Firestore", "firestore read fails");
                 }
+            }
+        });
+        */
+
+        firestore.collection("Doors").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot snapshots, @Nullable FirebaseFirestoreException e) {
+                if(e !=null){
+                    Log.d("listener","listener err",e);
+                    return;
+                }
+
+                for(DocumentChange dc:snapshots.getDocumentChanges()){
+                    switch(dc.getType()){
+                        case ADDED:
+                            Log.d("listener","add:" + dc.getDocument().getData());
+                            break;
+                        case REMOVED:
+                            Log.d("listener","removed:" + dc.getDocument().getData());
+                            break;
+                        case MODIFIED:
+                            Log.d("listener","modified:" + dc.getDocument().getData());
+                            break;
+                    }
+                }
+
             }
         });
     }
